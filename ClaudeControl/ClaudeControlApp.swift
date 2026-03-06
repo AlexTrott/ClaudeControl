b0b22e575f7ee17126bc7d339e1c81fc97051732
Alex Trott <hello@alextrott.me>
2026-03-06 19:44:33 +0000

    Initial Commit

---
+++ ClaudeControl/ClaudeControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeControlApp.swift
+//  ClaudeControl
+//
+//  Created by Alex Trott on 06/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
